b1e6fa60cde01c64907df263e1b62d1a5dec1498
Jiewen Yao <jiewen.yao@intel.com>
2018-05-11 22:49:21 -0700

    purley support edk2-minplatform

--- Platform/Intel/PurleyOpenBoardPkg/BoardMtOlympus/postbuild.bat
+++ Platform/Intel/PurleyOpenBoardPkg/BoardMtOlympus/postbuild.bat
@@ -17,29 +17,29 @@ set /a postbuildstep=0
 @echo.
 @echo BoardPostBuild.%postbuildstep% python PatchBinFv.py
 @set /a postbuildstep=%postbuildstep%+1
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvTempMemorySilicon
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvTempMemorySilicon
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvTempMemorySilicon
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvTempMemorySilicon
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo PatchBinFv Error. Exit
   goto :EOF
 )
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo PatchBinFv Error. Exit
   goto :EOF
 )
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo PatchBinFv Error. Exit
   goto :EOF
 )
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvLateSilicon
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvLateSilicon
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvLateSilicon
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvLateSilicon
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo PatchBinFv Error. Exit
@@ -49,16 +49,16 @@ if %ERRORLEVEL% NEQ 0 (
 @echo.
 @echo BoardPostBuild.%postbuildstep% python RebaseBinFv.py
 @set /a postbuildstep=%postbuildstep%+1
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspMBase
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspMBase
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspMBase
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPreMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspMBase
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo RebaseBinFv Error. Exit
   goto :EOF
 )
 
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspSBase
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspSBase
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspSBase
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\RebaseBinFv.py %TARGET% %WORKSPACE%\edk2-non-osi\Silicon\Intel\PurleySiliconBinPkg %WORKSPACE%\Build\BuildReport.txt FvPostMemorySilicon gMinPlatformModulePkgTokenSpaceGuid.PcdFlashFvFspSBase
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo RebaseBinFv Error. Exit
@@ -79,8 +79,8 @@ if %ERRORLEVEL% NEQ 0 (
 @echo.
 @echo BoardPostBuild.%postbuildstep% python PatchBfv.py
 @set /a postbuildstep=%postbuildstep%+1
-echo python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBfv.py %WORKSPACE%\Build\%BOARD_PKG%\%BOARD_NAME%\%TARGET%_%TOOL_CHAIN_TAG%\FV\PLATFORM.fd %WORKSPACE%\Build\BuildReport.txt gOpenBoardSupportPkgTokenSpaceGuid.PcdFlashFvPreMemoryBase
-call python %WORKSPACE%\edk2-platforms\Platform\Intel\MinPlatformModulePkg\Tools\PatchFv\PatchBfv.py %WORKSPACE%\Build\%BOARD_PKG%\%BOARD_NAME%\%TARGET%_%TOOL_CHAIN_TAG%\FV\PLATFORM.fd %WORKSPACE%\Build\BuildReport.txt gOpenBoardSupportPkgTokenSpaceGuid.PcdFlashFvPreMemoryBase
+echo python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBfv.py %WORKSPACE%\Build\%BOARD_PKG%\%BOARD_NAME%\%TARGET%_%TOOL_CHAIN_TAG%\FV\PLATFORM.fd %WORKSPACE%\Build\BuildReport.txt gOpenBoardSupportPkgTokenSpaceGuid.PcdFlashFvPreMemoryBase
+call python %WORKSPACE%\edk2-platforms\edk2-minplatform\MinPlatformModulePkg\Tools\PatchFv\PatchBfv.py %WORKSPACE%\Build\%BOARD_PKG%\%BOARD_NAME%\%TARGET%_%TOOL_CHAIN_TAG%\FV\PLATFORM.fd %WORKSPACE%\Build\BuildReport.txt gOpenBoardSupportPkgTokenSpaceGuid.PcdFlashFvPreMemoryBase
 if %ERRORLEVEL% NEQ 0 (
   set SCRIPT_ERROR=1
   echo PatchBfv Error. Exit
