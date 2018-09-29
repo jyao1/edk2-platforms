4baea3f8adf655978e592e67d2075ebc52004715
Jiewen Yao <jiewen.yao@intel.com>
2018-09-29 22:15:07 +0800

    Support Release build
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- Platform/Intel/MinPlatformPkg/bld.bat
+++ Platform/Intel/MinPlatformPkg/bld.bat
@@ -47,50 +47,63 @@ set openssl_path=%WORKSPACE%
 
 popd
 
-call build -p MinPlatformPkg\MinCorePkg.dsc -a IA32 -a X64 -t VS2015x86 -y Report.txt
+set TOOL_CHAIN_TAG=VS2015x86
+
+set TARGET=DEBUG
+call :Copy_BIN_FV
+
+set TARGET=RELEASE
+call :Copy_BIN_FV
+
+:done
+GOTO :EOF
+
+:Copy_BIN_FV
+
+call build -p MinPlatformPkg\MinCorePkg.dsc -a IA32 -a X64 -t %TOOL_CHAIN_TAG% -b %TARGET% -y Report.txt
 
 set CORE_FV_DIR=%WORKSPACE%\edk2-platforms\Platform\Intel\CoreFvBin
-set FV_BUILD_DIR=%WORKSPACE%\Build\MinCorePkg\DEBUG_VS2015x86\FV
+set FV_BUILD_DIR=%WORKSPACE%\Build\MinCorePkg\%TARGET%_%TOOL_CHAIN_TAG%\FV
 
-if not exist %CORE_FV_DIR%\Core\DEBUG (
-  mkdir %CORE_FV_DIR%\Core\DEBUG
+if not exist %CORE_FV_DIR%\Core\%TARGET% (
+  mkdir %CORE_FV_DIR%\Core\%TARGET%
 )
 
-copy %FV_BUILD_DIR%\FVPREMEMORYCORE.Fv %CORE_FV_DIR%\Core\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVPREMEMORYCORE.inf %CORE_FV_DIR%\Core\DEBUG\FVPREMEMORYCORE.inf /Y
+copy %FV_BUILD_DIR%\FVPREMEMORYCORE.Fv %CORE_FV_DIR%\Core\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVPREMEMORYCORE.inf %CORE_FV_DIR%\Core\%TARGET%\FVPREMEMORYCORE.inf /Y
 
-copy %FV_BUILD_DIR%\FVUEFIBOOTCORE.Fv %CORE_FV_DIR%\Core\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVUEFIBOOTCORE.inf %CORE_FV_DIR%\Core\DEBUG\FVUEFIBOOTCORE.inf /Y
+copy %FV_BUILD_DIR%\FVUEFIBOOTCORE.Fv %CORE_FV_DIR%\Core\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVUEFIBOOTCORE.inf %CORE_FV_DIR%\Core\%TARGET%\FVUEFIBOOTCORE.inf /Y
 
-copy %FV_BUILD_DIR%\FVOSBOOTCORE.Fv %CORE_FV_DIR%\Core\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVOSBOOTCORE.inf %CORE_FV_DIR%\Core\DEBUG\FVOSBOOTCORE.inf /Y
+copy %FV_BUILD_DIR%\FVOSBOOTCORE.Fv %CORE_FV_DIR%\Core\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVOSBOOTCORE.inf %CORE_FV_DIR%\Core\%TARGET%\FVOSBOOTCORE.inf /Y
 
-if not exist %CORE_FV_DIR%\UefiSecureBoot\DEBUG (
-  mkdir %CORE_FV_DIR%\UefiSecureBoot\DEBUG
+if not exist %CORE_FV_DIR%\UefiSecureBoot\%TARGET% (
+  mkdir %CORE_FV_DIR%\UefiSecureBoot\%TARGET%
 )
 
-copy %FV_BUILD_DIR%\FVUEFISECUREBOOT.Fv %CORE_FV_DIR%\UefiSecureBoot\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVUEFISECUREBOOT.inf %CORE_FV_DIR%\UefiSecureBoot\DEBUG\FVUEFISECUREBOOT.inf /Y
+copy %FV_BUILD_DIR%\FVUEFISECUREBOOT.Fv %CORE_FV_DIR%\UefiSecureBoot\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVUEFISECUREBOOT.inf %CORE_FV_DIR%\UefiSecureBoot\%TARGET%\FVUEFISECUREBOOT.inf /Y
 
-if not exist %CORE_FV_DIR%\Tcg\DEBUG (
-  mkdir %CORE_FV_DIR%\Tcg\DEBUG
+if not exist %CORE_FV_DIR%\Tcg\%TARGET% (
+  mkdir %CORE_FV_DIR%\Tcg\%TARGET%
 )
 
-copy %FV_BUILD_DIR%\FVTCG.Fv %CORE_FV_DIR%\Tcg\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVTCG.inf %CORE_FV_DIR%\Tcg\DEBUG\FVTCG.inf /Y
+copy %FV_BUILD_DIR%\FVTCG.Fv %CORE_FV_DIR%\Tcg\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVTCG.inf %CORE_FV_DIR%\Tcg\%TARGET%\FVTCG.inf /Y
 
-if not exist %CORE_FV_DIR%\Network\DEBUG (
-  mkdir %CORE_FV_DIR%\Network\DEBUG
+if not exist %CORE_FV_DIR%\Network\%TARGET% (
+  mkdir %CORE_FV_DIR%\Network\%TARGET%
 )
 
-copy %FV_BUILD_DIR%\FVUEFINETWORK.Fv %CORE_FV_DIR%\Network\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVUEFINETWORK.inf %CORE_FV_DIR%\Network\DEBUG\FVUEFINETWORK.inf /Y
+copy %FV_BUILD_DIR%\FVUEFINETWORK.Fv %CORE_FV_DIR%\Network\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVUEFINETWORK.inf %CORE_FV_DIR%\Network\%TARGET%\FVUEFINETWORK.inf /Y
 
-if not exist %CORE_FV_DIR%\Crypt\DEBUG (
-  mkdir %CORE_FV_DIR%\Crypt\DEBUG
+if not exist %CORE_FV_DIR%\Crypt\%TARGET% (
+  mkdir %CORE_FV_DIR%\Crypt\%TARGET%
 )
 
-copy %FV_BUILD_DIR%\FVCRYPT.Fv %CORE_FV_DIR%\Crypt\DEBUG\ /Y
-copy %FV_BUILD_DIR%\AsBuildFVCRYPT.inf %CORE_FV_DIR%\Crypt\DEBUG\FVCRYPT.inf /Y
+copy %FV_BUILD_DIR%\FVCRYPT.Fv %CORE_FV_DIR%\Crypt\%TARGET%\ /Y
+copy %FV_BUILD_DIR%\AsBuildFVCRYPT.inf %CORE_FV_DIR%\Crypt\%TARGET%\FVCRYPT.inf /Y
 
 goto :EOF
